6f04de4b4dc627219c09c00a7d69a05b9c74d380
azeral <3231202800@qq.com>
2023-08-11 13:56:52 +0800

    add foreign key

--- db/comment.go
+++ db/comment.go
@@ -10,6 +10,7 @@ import (
 type Comment struct {
 	ID        int64     `gorm:"primary_key;type:bigint;auto_increment;comment:评论id"`
 	UserID    int64     `gorm:"type:bigint;comment:用户id"`
+	User      User      `gorm:"foreignKey:UserID;references:ID;association_autoupdate:false;association_autocreate:false;comment:用户信息"`
 	VideoID   int64     `gorm:"type:bigint;comment:视频id"`
 	Content   string    `gorm:"type:varchar(500);comment:评论内容"`
 	CreatedAt time.Time `gorm:"comment:创建时间"`
