2175ac2898e7ba5c07752901559f2c573c5236f2
azeral <3231202800@qq.com>
2023-08-11 15:23:48 +0800

    comment model

--- model/comment.go
+++ model/comment.go
@@ -6,19 +6,14 @@ type CommentActionResponse struct {
 }
 
 type CommentActionRequest struct {
-	Token      string `json:"token"`
-	VideoId    int64  `json:"video_id"`
-	ActionType int32  `json:"action_type"`            // 1 - post, 2 - delete
-	Content    string `json:"comment_text,omitempty"` // used when action_type = 1
-	CommentId  int64  `json:"comment_id,omitempty"`   // used when action_type = 2
+	Token      string `json:"token" form:"token" xml:"token" binding:"required"`
+	VideoId    int64  `json:"video_id" form:"video_id" xml:"video_id" binding:"required"`
+	ActionType int32  `json:"action_type" form:"action_type" xml:"action_type" binding:"required,oneof=1 2"` // 1 - post, 2 - delete
+	Content    string `json:"comment_text,omitempty" form:"comment_text" xml:"comment_text"`                 // used when action_type = 1
+	CommentId  int64  `json:"comment_id,omitempty" form:"comment_id" xml:"comment_id"`                       // used when action_type = 2
 }
 
 type CommentListResponse struct {
 	Response
 	CommentList []Comment `json:"comment_list"`
-}
-
-type CommentListRequest struct {
-	Token   string `json:"token"`
-	VideoId int64  `json:"video_id"`
 }
